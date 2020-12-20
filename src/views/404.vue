1b366739be82f28658c021fe09723aa75b5ccbc4
SpringSheep <996800597@qq.com>
2020-12-20 21:34:00 +0800

    404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>404</div>
-    
+  <a-result status="404" title="404" sub-title="对不起，你访问的页面丢了。">
+    <template #extra>
+      <a-button type="primary" @click="go"> 返回首页 </a-button>
+    </template>
+  </a-result>
 </template>
 <script>
 export default {
-    
+  methods: {
+    go() {
+      const loginURL = `http://${
+        window.location.hostname !== '172.23.2.17'
+          ? '49.235.30.187'
+          : '172.23.2.17'
+      }:8088/auth/#/user/login?from=${encodeURIComponent(location.href)}`
+      window.location.href = loginURL
+    },
+  },
 }
 </script>
-<style lang="less" scoped>
-
-</style>
