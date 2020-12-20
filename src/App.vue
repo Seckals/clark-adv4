fbaa6209a70ef113bc7b32a48009d327748de867
SpringSheep <996800597@qq.com>
2020-12-20 17:50:11 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ export default {
         window.location.hostname !== '172.23.2.17'
           ? '49.235.30.187'
           : '172.23.2.17'
-      }:8088/auth/#/login?from=${encodeURIComponent(location.href)}`
+      }:8088/auth/#/user/login?from=${encodeURIComponent(location.href)}`
       window.location.href = loginURL
     },
   },
